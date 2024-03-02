d668bc9b27f681a71c8000b64fb95f11f5ac919c
Karma-David <quytre19042003@gmail.com>
2024-03-02 21:06:34 +0700

    change hello sout

--- src/main/java/Controller/Hello.java
+++ src/main/java/Controller/Hello.java
@@ -9,7 +9,7 @@ package Controller;
  * @author Manh
  */
 public class Hello {
-    public static void main(Striádasdg[] args)s {
+    public static void main(String[] args) {
         System.out.println("");
-   }
+    }
 }
